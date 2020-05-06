74e8779d31673327d3473e2c07cc9001bbea40bf
Json Bourne <jsonbourneagain@outlook.com>
2020-05-07 02:13:35 +0530

    Solved

---
+++ DataStructuresAndAlgorithms/HashTables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool CanTwoMoviesFillFlight(int[] movieLengths, int flightLength)
+        {
+            // Determine if two movies add up to the flight length
+            var seenMoviesLengths = new HashSet<int>();
+
+            foreach (var firstMovieLength in movieLengths)
+            {
+                var matchingSecondMovieLength = flightLength - firstMovieLength;
+
+                if (seenMoviesLengths.Contains(matchingSecondMovieLength))
+                {
+                    return true;
+                }
+
+                seenMoviesLengths.Add(firstMovieLength);
+            }
+
+            return false;
+        }
+
+    }
+
+}
