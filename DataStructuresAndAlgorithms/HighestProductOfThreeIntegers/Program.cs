5a2be400a079c73eacb0d471d3d0127fa56bbd76
Json Bourne <jsonbourneagain@outlook.com>
2020-05-13 21:22:10 +0530

    Corrected a logical error

--- DataStructuresAndAlgorithms/HighestProductOfThreeIntegers/Program.cs
+++ DataStructuresAndAlgorithms/HighestProductOfThreeIntegers/Program.cs
@@ -6,7 +6,7 @@ namespace HighestProductOfThreeIntegers
     {
         static void Main(string[] args)
         {
-            var actual = HighestProductOf3(new int[] { 1, 2, 3, 4 });
+            var actual = HighestProductOf3(new int[] { -10, -10, 1, 3, 4 });
 
             Console.WriteLine("Hello World!");
         }
@@ -19,26 +19,39 @@ namespace HighestProductOfThreeIntegers
                 throw new ArgumentException("We need minimum 3 elements in ", nameof(arrayOfInts));
             }
 
-            var max3Integers = new int[] { arrayOfInts[0], arrayOfInts[1], arrayOfInts[2] };
-            Array.Sort(max3Integers); // Sorted in ascending order.
+            int highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+            int highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+            int lowestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+            int highest = Math.Max(arrayOfInts[0], arrayOfInts[1]);
+            int lowest = Math.Min(arrayOfInts[0], arrayOfInts[1]);
 
-            for (int i = 3; i < arrayOfInts.Length; i++)
+            for (int i = 2; i < arrayOfInts.Length; i++)
             {
-                if (arrayOfInts[i] > max3Integers[0]) // we know that we can get maxProductOf3 using this ith number.
+                var current = arrayOfInts[i];
+
+                if (current * lowestProductOf2 > highestProductOf3)
                 {
-                    max3Integers[0] = arrayOfInts[i];
-                    Array.Sort(max3Integers);
+                    highestProductOf3 = current * lowestProductOf2;
                 }
+                if (current * highestProductOf2 > highestProductOf3)
+                {
+                    highestProductOf3 = current * highestProductOf2;
+                }
+
+                if (highest * current > highestProductOf2)
+                {
+                    highestProductOf2 = highest * current;
+                }
+                if (lowest * current < lowestProductOf2)
+                {
+                    lowestProductOf2 = lowest * current;
+                }
+
+                highest = Math.Max(highest, current);
+                lowest = Math.Min(lowest, current);
             }
 
-            int maxProduct = 1;
-
-            foreach (var item in max3Integers)
-            {
-                maxProduct *= item;
-            }
-
-            return maxProduct;
+            return highestProductOf3;
         }
     }
 }
